bf3ddf585dc8a845886196bd9bfa20c9af521a5e
Arash Shalchian <shalchianarash@gmail.com>
2024-09-02 17:00:53 -0400

    Changed some styles and functionalities in the hero section

--- trainer-app-frontend/src/Components/Hero/Hero.js
+++ trainer-app-frontend/src/Components/Hero/Hero.js
@@ -1,29 +1,34 @@
 import React from "react";
-import { useNavigate } from "react-router-dom";
 import "./Hero.css";
+import Logo from "./../../Assets/Images/logo2.png"; // Update with the actual path to your logo
+import { FaArrowDown } from "react-icons/fa"; // Import the icon from react-icons
+import BackgroundImage from "./../../Assets/Images/bg-hero.jpg"; // Update with the actual path to your background image
 
 function Hero() {
-  const navigate = useNavigate();
+  const scrollDown = () => {
+    window.scrollBy({ top: window.innerHeight * 0.75, behavior: "smooth" });
+  };
 
   return (
-    <section className="hero">
-      <div className="hero-content">
-        <h1 className="hero-title">FitSho</h1>
-        <p className="hero-subtitle">
-          Your journey to a healthier, fitter life starts here.
-        </p>
-        <div className="hero-buttons">
+    <section
+      className="hero"
+      style={{ backgroundImage: `url(${BackgroundImage})` }}
+    >
+      <div className="hero-container">
+        <div className="hero-content">
+          <div className="hero-title-container">
+            <img src={Logo} alt="FitSho Logo" className="hero-logo" />
+            <h1 className="hero-title">FitSho</h1>
+          </div>
+          <p className="hero-subtitle">
+            Your journey to a healthier, fitter life starts here.
+          </p>
           <button
-            className="hero-button start-now-button"
-            onClick={() => navigate("/programs")}
+            className="hero-scroll-button"
+            onClick={scrollDown}
+            aria-label="Scroll down to the next section"
           >
-            Start Now
-          </button>
-          <button
-            className="hero-button learn-more-button"
-            onClick={() => navigate("/programs#learn-more")}
-          >
-            Learn More
+            <FaArrowDown />
           </button>
         </div>
       </div>
