ffea82f9af6420bc95e6ec6a78090e0e273401c5
Arash Shalchian <shalchianarash@gmail.com>
2024-09-04 02:28:55 -0400

    Added some changes to the Hero section

--- trainer-app-frontend/src/Components/Hero/Hero.js
+++ trainer-app-frontend/src/Components/Hero/Hero.js
@@ -1,12 +1,19 @@
 import React from "react";
+import { useNavigate } from "react-router-dom"; // Import useNavigate hook
 import "./Hero.css";
-import Logo from "./../../Assets/Images/logo2.png"; // Update with the actual path to your logo
-import { FaArrowDown } from "react-icons/fa"; // Import the icon from react-icons
-import BackgroundImage from "./../../Assets/Images/bg-hero.jpg"; // Update with the actual path to your background image
+import Logo from "./../../Assets/Images/logo2.png";
+import { FaArrowDown } from "react-icons/fa";
+import BackgroundImage from "./../../Assets/Images/bg-hero.jpg";
 
 function Hero() {
+  const navigate = useNavigate(); // Initialize the navigate function
+
   const scrollDown = () => {
     window.scrollBy({ top: window.innerHeight * 0.75, behavior: "smooth" });
+  };
+
+  const goToPrograms = () => {
+    navigate("/programs"); // Navigate to the /programs page when button is clicked
   };
 
   return (
@@ -20,9 +27,12 @@ function Hero() {
             <img src={Logo} alt="FitSho Logo" className="hero-logo" />
             <h1 className="hero-title">FitSho</h1>
           </div>
-          <p className="hero-subtitle">
+          <p className="hero-subtitle animated-text">
             Your journey to a healthier, fitter life starts here.
           </p>
+          <button className="cta-button" onClick={goToPrograms}>
+            Join Now
+          </button>
           <button
             className="hero-scroll-button"
             onClick={scrollDown}
