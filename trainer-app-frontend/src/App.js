38c448c1968ccce0f1458ec77d3ea15acfd7a81f
Arash Shalchian <shalchianarash@gmail.com>
2024-09-09 16:23:29 -0400

    Added the about route

--- trainer-app-frontend/src/App.js
+++ trainer-app-frontend/src/App.js
@@ -19,7 +19,7 @@ function App() {
           <Route path="/login" element={<Login />} /> {/* Login page */}
           <Route path="/signup" element={<Signup />} /> {/* Signup route */}
           <Route path="/profile" element={<Profile />} /> {/* Profile page */}
-          <Route path="About" element={<About />} /> {/* About page */}
+          <Route path="/about" element={<About />} /> {/* About page */}
         </Routes>
         <Footer />
       </div>
